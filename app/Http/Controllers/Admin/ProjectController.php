1b2002211c0ca0cac3cff7a8c9ae23a132ffae02
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-08-22 17:08:55 +0200

    passed dataabse infos to the view

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
@@ -12,7 +13,9 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        return view('admin.projects.index');
+        $projects = Project::paginate(10);
+
+        return view('admin.projects.index', compact('projects'));
     }
 
     /**
