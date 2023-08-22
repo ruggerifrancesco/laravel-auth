2f328fa78fb228eca37fc4ade5419953c0df0465
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-08-22 11:19:03 +0200

    Updated routes and views to have more stable project

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
+use App\Http\Controllers\Guest\GuestController as GuestHomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,14 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Auth::routes();
+
+Route::prefix('admin')->name('admin.')->middleware('auth')->group(function () {
+    Route::get('/', [ AdminDashboardController::class , 'home'])->name('home');
 });
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::name('guest.')->group(function () {
+    Route::get('/', [ GuestHomeController::class , 'home'])->name('home');
+});
